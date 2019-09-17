ed360349eeb0072cd65584ca44593cda9b79678f
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2019-09-17 15:00:36 +0200

    Update __init__.py

--- yolo/vedanet/network/head/__init__.py
+++ yolo/vedanet/network/head/__init__.py
@@ -4,8 +4,6 @@ from ._region_mobilenet import *
 from ._region_mobilenetv2 import *
 from ._tiny_yolov2 import *
 from ._tiny_yolov3 import *
-from ._region_shufflenet import *
-from ._region_shufflenetv2 import *
 from ._region_squeezenext import *
 from ._region_xception import *
 from ._region_light_xception import *
