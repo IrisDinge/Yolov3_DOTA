a8466b3ad995137ed441bfe1ddc9d18a3a3e0620
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2019-09-28 00:40:55 +0200

    Update darknet53.py

--- yolo/vedanet/network/backbone/brick/darknet53.py
+++ yolo/vedanet/network/backbone/brick/darknet53.py
@@ -102,10 +102,67 @@ class Transition(nn.Module):
     def forward(self, data):
         x = self.features(data)
         return x
-    
-    
 
 
-    
-    
+class SPPBody(nn.Module):
+    '''
 
+    5 * DBL: (1-3) + spp +(4-5)
+
+    '''
+
+    custom_layers = ()
+
+    def __init__(self, nchannels, first_head=False):
+        super().__init__()
+        if first_head:
+            half_nchannels = int(nchannels / 2)
+        else:
+            half_nchannels = int(nchannels / 3)
+        in_nchannels = 2 * half_nchannels
+        layers = [
+            vn_layer.Conv2dBatchLeaky(nchannels, half_nchannels, 1, 1),
+            vn_layer.Conv2dBatchLeaky(half_nchannels, in_nchannels, 3, 1),
+            vn_layer.Conv2dBatchLeaky(in_nchannels, half_nchannels, 1, 1),
+            vn_layer.SPPLayer(3, pool_type='max_pool'), #num_level=3
+            vn_layer.Conv2dBatchLeaky(half_nchannels, in_nchannels, 3, 1),
+            vn_layer.Conv2dBatchLeaky(in_nchannels, half_nchannels, 1, 1)
+        ]
+        self.feature = nn.Sequential(*layers)
+
+    def forward(self, data):
+        x = self.feature(data)
+        return x
+    
+    
+class DCNv2(nn.Module):
+
+    '''
+
+    Deformable Convolutional Networks
+
+    '''
+
+
+    def __init__(self):
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d((2, 2))
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = vn_layer.DeformConv2(32, 64, 3, padding=1, modulation=True)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.fc = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = self.relu(self.bn2(self.conv2(x)))
+
+        x = self.avg_pool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+
+        return x
