7571f0b192ee74bcd49a8b0f5f9d88567e686c1d
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2019-10-20 12:01:09 +0200

    Update _darknet.py

--- yolo/vedanet/network/layer/_darknet.py
+++ yolo/vedanet/network/layer/_darknet.py
@@ -598,10 +598,13 @@ class Conv2dBatchReLU(nn.Module):
 
 class SPPLayer(torch.nn.Module):
 
+    
+class SPPLayer(torch.nn.Module):
+
     def __init__(self, num_levels, pool_type='max_pool'):
         super(SPPLayer, self).__init__()
 
-        self.num_levels = num_levels
+        self.num_levels = num_levels  # 4 target = [num, 13 x 13 x 2048]
         self.pool_type = pool_type
 
     def forward(self, x):
@@ -612,45 +615,10 @@ class SPPLayer(torch.nn.Module):
         # w: width
         n, c, h, w = x.size()
         #         print(x.size())
-        for i in range(self.num_levels):
-            level = i + 1
-
-            '''
-            The equation is explained on the following site:
-            http://www.cnblogs.com/marsggbo/p/8572846.html#autoid-0-0-0
-            '''
-            kernel_size = (math.ceil(h / level), math.ceil(w / level))
-            stride = (math.floor(h / level), math.floor(w / level))
-            pooling = (
-            math.floor((kernel_size[0] * level - h + 1) / 2), math.floor((kernel_size[1] * level - w + 1) / 2))
-
-            # update input data with padding
-            zero_pad = torch.nn.ZeroPad2d((pooling[1], pooling[1], pooling[0], pooling[0]))
-            x_new = zero_pad(x)
-
-            # update kernel and stride
-            h_new = 2 * pooling[0] + h
-            w_new = 2 * pooling[1] + w
-
-            kernel_size = (math.ceil(h_new / level), math.ceil(w_new / level))
-            stride = (math.floor(h_new / level), math.floor(w_new / level))
-
-            # 选择池化方式
-            if self.pool_type == 'max_pool':
-                try:
-                    tensor = F.max_pool2d(x_new, kernel_size=kernel_size, stride=stride).view(n, -1)
-                except Exception as e:
-                    print(str(e))
-                    print(x.size())
-                    print(level)
-            else:
-                tensor = F.avg_pool2d(x_new, kernel_size=kernel_size, stride=stride).view(n, -1)
-
-            # 展开、拼接
-            if (i == 0):
-                x = tensor.view(n, -1)
-            else:
-                x = torch.cat((x, tensor.view(n, -1)), 1)
+        a = (self.num_levels -1) * 2
+        zero_pad = torch.nn.ZeroPad2d((a, a, a, a))
+        x = zero_pad(x)
+        
         return x
 
 
